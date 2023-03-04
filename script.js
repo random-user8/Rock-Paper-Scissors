0abea8bc0b7a9e6acb616a64d30cadad326d12bb
random-user8 <random.user8@yandex.com>
2023-03-04 14:25:48 +0100

    Add playRound() function

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
-function getComputerChoice(){
+function getComputerChoice() {
     let random = Math.floor(Math.random() * 3);
     let string = ['Rock', 'Paper', 'Scissors'];
     let result = string[random];
     return result;
+}
+
+function playRound(playerSelection, computerSelection) {
+    
+    let toLowerPlayer = playerSelection.toLowerCase();
+    let toLowerComputer = computerSelection.toLowerCase();
+
+    if ((toLowerPlayer === 'rock') && (toLowerComputer === 'scissors') ) {
+        console.log('You win! Rock beats Scissors');
+    } else if ((toLowerPlayer === 'paper') && (toLowerComputer === 'rock') ) {
+        console.log('You win! Paper beats Rock.');
+    } else if ((toLowerPlayer === 'Scissors') && (toLowerComputer === 'Paper')) {
+        console.log('You win! Scissors beats Paper.');
+    } else if ((toLowerPlayer === 'rock') && (toLowerComputer === 'paper') ) {
+        console.log('You lose! Paper beats Rock.');
+    } else if ((toLowerPlayer === 'Paper') && (toLowerComputer === 'Scissors') ) {
+        console.log('You lose! Scissors beats Paper.');
+    } else if ((toLowerPlayer === 'scissors') && (toLowerComputer === 'rock')) {
+        console.log('You win! Rock beats Scissors.');
+    }
 }
