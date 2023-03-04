31197812a3002e09c05a30ad296a4dc4f0d426ba
random-user8 <random.user8@yandex.com>
2023-03-04 14:43:24 +0100

    Modify playRound() function with "return"

--- script.js
+++ script.js
@@ -6,21 +6,21 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    
+
     let toLowerPlayer = playerSelection.toLowerCase();
     let toLowerComputer = computerSelection.toLowerCase();
 
     if ((toLowerPlayer === 'rock') && (toLowerComputer === 'scissors') ) {
-        console.log('You win! Rock beats Scissors');
+        return 'You win! Rock beats Scissors';
     } else if ((toLowerPlayer === 'paper') && (toLowerComputer === 'rock') ) {
-        console.log('You win! Paper beats Rock.');
+        return 'You win! Paper beats Rock.';
     } else if ((toLowerPlayer === 'Scissors') && (toLowerComputer === 'Paper')) {
-        console.log('You win! Scissors beats Paper.');
+        return 'You win! Scissors beats Paper.';
     } else if ((toLowerPlayer === 'rock') && (toLowerComputer === 'paper') ) {
-        console.log('You lose! Paper beats Rock.');
+        return 'You lose! Paper beats Rock.';
     } else if ((toLowerPlayer === 'Paper') && (toLowerComputer === 'Scissors') ) {
-        console.log('You lose! Scissors beats Paper.');
+        return 'You lose! Scissors beats Paper.';
     } else if ((toLowerPlayer === 'scissors') && (toLowerComputer === 'rock')) {
-        console.log('You win! Rock beats Scissors.');
+        return 'You win! Rock beats Scissors.';
     }
 }
